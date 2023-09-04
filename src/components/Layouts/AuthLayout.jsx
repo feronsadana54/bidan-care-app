6915bc496e6eca9ea93a6aaa6db9e7274d9ba691
Feron Sadana <feronsadana16@gmail.com>
2023-09-04 17:00:40 +0700

    initial files

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,3 +1,5 @@
+import LogoImage from "./../../assets/img/BidanCareLogo.png";
+
 /* eslint-disable react/prop-types */
 export default function AuthLayout(props) {
   const { children, judul } = props;
@@ -5,7 +7,7 @@ export default function AuthLayout(props) {
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <div className="w-full max-w-md p-4 bg-white rounded-lg shadow dark:border dark:border-gray-700">
         <div className="w-full">
-          <img src="/src/assets/img/BidanCareLogo.png" alt="Logo" />
+          <img src={LogoImage} alt="Logo" />
         </div>
         <div className="p-6 space-y-4">
           <h1 className="text-xl font-bold leading-tight text-gray-900 dark:text-white">
